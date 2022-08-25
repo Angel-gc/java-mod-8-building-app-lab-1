f00f7a615095d311bb11aa4064717a2615e9c5a6
Angel Concepcion <angelconcepcion50@gmail.com>
2022-08-25 17:20:36 -0400

    comments added

--- src/app/user-message-component/user-message-component.component.ts
+++ src/app/user-message-component/user-message-component.component.ts
@@ -1,7 +1,7 @@
 import { MessagingDataService } from './../messaging-data.service';
 import { Message } from './../message.model';
 import { Component, Input, OnInit } from "@angular/core";
-import { User } from '../user.model';
+
 
 @Component({
   selector: "app-user-message-component",
@@ -9,6 +9,8 @@ import { User } from '../user.model';
   styleUrls: ["./user-message-component.component.css"],
 })
 export class UserMessageComponentComponent implements OnInit {
+  //decorator @Input() that is sharing data with its parent component:
+  //conversation thread component
   @Input() 
   message: Message 
   id: number;
